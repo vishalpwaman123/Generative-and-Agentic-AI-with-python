391257533b3fa601e91064156992b0a68e538a91
vishalwamanmusafir <vishal.waman@in.musafir.com>
2025-12-31 15:09:11 +0530

    Added : function examples

---
+++ Functions/function1.py
@@ -0,0 +1,6 @@
+def print_order(name, chai_type):
+    print(f"{name} ordered {chai_type} chai")
+
+print_order("Vishal", "masala")
+print_order("rahul", "ginger")
+print_order("kajal", "tulsi")
---
+++ Functions/function2.py
@@ -0,0 +1,16 @@
+def fetch_data():
+    print("fetching the sales data")
+
+def filter_valid_sales():
+    print("Filtering valid sales data")
+
+def summerize_data():
+    print("Summerizing sales data")
+
+def generate_report():
+    fetch_data()
+    filter_valid_sales()
+    summerize_data()
+    print("Report is ready")
+
+generate_report()
