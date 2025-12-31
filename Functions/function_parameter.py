29963ddc28c18742c04e0fcf70513a448f058912
vishalwamanmusafir <vishal.waman@in.musafir.com>
2025-12-31 17:47:18 +0530

    Added : Arguments In function

---
+++ Functions/function_parameter.py
@@ -0,0 +1,36 @@
+# EXAMPLE 1
+
+chai = "Ginger chai"
+
+def prepare_chai(order):
+    print("Preparing : ",order)
+
+prepare_chai(chai)
+print(chai)
+
+######################### EXAMPLE 2 ########################
+
+chai = [1, 2, 3]
+
+def edit_chai(cup):
+    cup[1] = 42 # [1, 42, 3]
+
+edit_chai(chai)
+print(chai)
+
+######################### EXAMPLE 3 #########################
+
+def make_chai(tea, milk, sugar):
+    print(tea, milk, sugar)
+
+make_chai("Darjeeling", "Yes", "Low") #POSITIONAL
+make_chai(tea="Green", sugar="Medium", milk="No")
+
+
+####################### EXAMPLE 4 ###########################
+
+def special_chai(*ingredients, **extras):
+    print("Ingredients : ", ingredients)
+    print("Extra : ",extras)
+
+special_chai("Masala", "Irani", sweetener="Honey", form="yes")
