332a8916b33aca696811a21d2879d211ded57e0c
vishalwamanmusafir <vishal.waman@in.musafir.com>
2026-03-28 13:08:46 +0530

    Create : Basic AI Project

---
+++ GenAI/02_basic/openai_basic.py
@@ -0,0 +1,16 @@
+# Key : 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
+
+from openai import OpenAI
+
+client = OpenAI(
+  api_key="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"
+)
+
+response = client.responses.create(
+  model="gpt-5-nano",
+  input="hi",
+  store=True,
+)
+
+print(response.output_text);
+
