ada7b12b61be6a50f84f6352158e3b329f6b6690
vishalwamanmusafir <vishal.waman@in.musafir.com>
2026-03-28 17:38:21 +0530

    Added : Gemini code

---
+++ GenAI/02_basic/google_genai_basic.py
@@ -0,0 +1,11 @@
+from google import genai
+
+# The client gets the API key from the environment variable `GEMINI_API_KEY`.
+client = genai.Client(
+    api_key = "GEMINI_SECRET_EKY"
+)
+
+response = client.models.generate_content(
+    model="gemini-3-flash-preview", contents="Hi, Vishal This side."
+)
+print(response.text)
